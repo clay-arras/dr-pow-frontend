c4c1c9d707adb662f609f4c9f9fe2ac6bfb8e41a
liminghao <lmh1230612306@163.com>
2024-01-21 07:50:43 -0800

    Adjusted layout 2

--- src/components/BigArea.js
+++ src/components/BigArea.js
@@ -1,37 +1,57 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "../css/BigComponent.css";
-import { TemplateSelectorArea } from "./TemplateSelectorArea/TemplateSelectorArea";
-import { UploadArea } from "./UploadArea/UploadArea";
+import {TemplateSelectorArea} from "./TemplateSelectorArea/TemplateSelectorArea";
+import {UploadArea} from "./UploadArea/UploadArea";
 import title from "../images/title-image.png";
+import {Button, Popover, PopoverContent, PopoverTrigger} from "@nextui-org/react";
 
 export function BigArea() {
-  const [selectedTemplate, setSelectedTemplate] = useState("Portfolio");
+    const [selectedTemplate, setSelectedTemplate] = useState("Portfolio");
 
-  return (
-    <div className={"main-container"}>
-        <div className={"title-section"}>
-            <img src={title} alt="logo" className={"title-image"}/>
+    return (
+        <div className={"main-container"}>
+            <div className={"title-section"}>
+                <img src={title} alt="logo" className={"title-image"}/>
 
-            <div className={"title"}>
-                <h1>DoC-To-POW!</h1>
-                <h3 className={"sub-header"}>
-                    YOUR #1 MULTI-LANGUAGE AI POWERPOINT ASSISTANT
-                </h3>
+                <div className={"title"}>
+                    <h1>DoC-To-POW!</h1>
+                    <h3 className={"sub-header"}>
+                        YOUR #1 MULTI-LANGUAGE AI POWERPOINT ASSISTANT
+                    </h3>
+                </div>
+            </div>
+            <div className={"upload-section"}>
+                <div className={"title-section"}>
+                    <div>
+                        <h2>Create Your Powerpoint!</h2>
+                    </div>
+                    <div className={"choose-template-button"}>
+                        <Popover placement="top-end">
+                            <PopoverTrigger>
+                                <Button isIconOnly>
+                                    <QuestionMark/>
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <div className="px-1 py-2">
+                                    <div className="text-small font-bold">Popover Content</div>
+                                    <div className="text-tiny">This is the popover content</div>
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+                    </div>
+                </div>
+                <UploadArea
+                    selectedTemplate={selectedTemplate}
+                    setSelectedTemplate={setSelectedTemplate}
+                />
+            </div>
+            <div className={"theme-section"}>
+                <TemplateSelectorArea
+                    selectedTemplate={selectedTemplate}
+                    setSelectedTemplate={setSelectedTemplate}
+                />
             </div>
         </div>
-        <div className={"upload-section"}>
-            <h2>Create Your Powerpoint!</h2>
-        <UploadArea
-          selectedTemplate={selectedTemplate}
-          setSelectedTemplate={setSelectedTemplate}
-        />
-      </div>
-      <div className={"theme-section"}>
-        <TemplateSelectorArea
-          selectedTemplate={selectedTemplate}
-          setSelectedTemplate={setSelectedTemplate}
-        />
-      </div>
-    </div>
-  );
+    );
 }
